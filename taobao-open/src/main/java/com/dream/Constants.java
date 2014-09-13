b0333403861e58992ad8b513f9a6a4ac67080b3d
Frank <f.46886799@gmail.com>
2014-09-13 08:26:51 +0800

    commit
    commit

---
+++ taobao-open/src/main/java/com/dream/Constants.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.dream;
+
+
+/**
+ * @author Frank
+ *
+ */
+public class Constants {
+	
+	public static final String OAUTH_TOKEN_URL="https://oauth.tbsandbox.com/token";
+	public static final String OAUTH_AUTH_URL="https://oauth.tbsandbox.com/token";
+	
+	public static final String TOP_URL="http://gw.api.tbsandbox.com/router/rest";
+	
+	public static final String APP_KEY = "1021697746";
+	public static final String APP_SECRET = "sandbox18b7c5cc19a602a635ebb8bcd";
+	public static final String GROUP_NAME = "default";
+}
